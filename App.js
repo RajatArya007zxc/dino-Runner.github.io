ce0ca0a8eac696ffb20f9f5373b351f5f897c508
RajatArya007zxc <rajatarya199zx@gmail.com>
2021-03-14 15:37:58 +0530

    adding functionality of jumping and get back to the ground

--- App.js
+++ App.js
@@ -1,17 +1,48 @@
 const dino=document.querySelector('.dino')
-console.log(dino);
+let isJumping=false;
 
 const control=(e)=>{
 
      if(e.keyCode===32){
+         if(!isJumping){
+ isJumping=true;
+ jumpingFunction();
+         }
         
-        jumpingFunction();
+        
 
      }
 }
 document.addEventListener('keyup',control);
 
 function jumpingFunction(){
+
+    let position=0;
+
+    const timerId=setInterval(function(){
+
+       if(position===150){
+           clearInterval(timerId);
+
+           ///dino down
+
+          const downInterval=setInterval(function(){
+  
+            if(position===0) {
+                clearInterval(downInterval)
+                isJumping=false;
+            }
+            position-=30;
+            dino.style.bottom=position+'px';
+          },20)
+       }
+         
+        ///dino up
+        console.log('up')
+        position+=30;
+        dino.style.bottom=position+'px';
+
+    },20)
 
     
 }
